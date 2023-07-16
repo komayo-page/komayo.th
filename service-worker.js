e3880b437dfcc356b46fc8775a76fb9c9f00c634
Cyril Kato <contact@cyril.email>
2023-07-16 16:15:10 +0200

    feat: new version

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,22 @@
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('v1').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/css/pico.classless.min.css',
-        '/favicon-512x512.png',
-        '/favicon.svg',
-        '/manifest.json',
-        '/koo-maa-yo-yim-hai-gap-sat-ruu-tii-lap-l-i.png',
-        '/koo-maa-yo-yim-hai-gap-sat-ruu-tii-lap-l-i.webp',
-      ]);
-    })
-  );
+event.waitUntil(
+caches.open('v1').then((cache) => {
+return cache.addAll([
+'/',
+'/css/pico.classless.min.css',
+'/favicon-512x512.png',
+'/favicon.svg',
+'/manifest.json',
+'/koo-maa-yo-yim-hai-gap-sat-ruu-tii-lap-l-i.png',
+'/koo-maa-yo-yim-hai-gap-sat-ruu-tii-lap-l-i.webp',
+]);
+})
+);
 });
-
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+event.respondWith(
+caches.match(event.request).then((response) => {
+return response || fetch(event.request);
+})
+);
 });
